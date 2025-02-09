d0d7193c0e64a8b09fe5586f6a70523114a361ad
Corin Craciun <craciuncorin5@gmail.com>
2025-02-09 20:45:58 +0100

    updated tmux.conf

--- private_dot_config/nvim/lua/core/options.lua
+++ private_dot_config/nvim/lua/core/options.lua
@@ -1,16 +1,16 @@
 --Set general options
-vim.wo.number = true  --line numbers enabled
-vim.o.relativenumber = true  --relative number
-vim.o.clipboard = 'unnamedplus'  --sync clipboard between os
-vim.o.wrap = false  --display lines as one long line (default true)
+vim.wo.number = true --line numbers enabled
+vim.o.relativenumber = true --relative number
+vim.o.clipboard = 'unnamedplus' --sync clipboard between os
+vim.o.wrap = false --display lines as one long line (default true)
 vim.o.linebreak = true --don't split words
 vim.o.mouse = 'a' --enables mouse
 vim.o.autoindent = true
 vim.o.ignorecase = true --case-insensitive search unless \C when searching
 vim.o.smartcase = true
-vim.o.shiftwidth = 4  --default 8
-vim.o.tabstop = 4 --default 8
-vim.o.softtabstop = 4 --default 8 (number of staces a tab represents)
+vim.o.shiftwidth = 2 --default 8
+vim.o.tabstop = 2 --default 8
+vim.o.softtabstop = 2 --default 8 (number of staces a tab represents)
 vim.o.cursorline = false -- highlight the current line
 vim.o.splitbelow = true -- force all horizontal splits to go below current window
 vim.o.splitright = true -- force all vertical splits to go to the right of current window
@@ -28,3 +28,4 @@ vim.opt.shortmess:append 'c' -- don't give |ins-completion-menu| messages
 vim.opt.iskeyword:append '-' -- hyphenated words recognized by searches
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- separate vim plugins from neovim in case vim still in use
+vim.o.autowriteall = true --autosaves files
